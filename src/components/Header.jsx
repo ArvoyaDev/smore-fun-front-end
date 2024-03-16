cafd7c735a86d58a87b10087d31e1478db529d37
Isai Chaidez <isaichaidez@gmail.com>
2024-03-16 16:59:32 -0700

    website is now mobile friendly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,94 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { Offcanvas } from 'react-bootstrap';
 import logo from '../../public/assests/SVG/logo-green.svg';
-import {withAuth0 } from "@auth0/auth0-react";
+import { withAuth0 } from "@auth0/auth0-react";
 import Login from "./Login.jsx";
 import Logout from "./Logout.jsx";
 
-
-
 class Header extends React.Component {
+	state = {
+		showOffcanvas: false,
+		windowWidth: window.innerWidth
+	}
 
-render() {
-	const { user, isAuthenticated } = this.props.auth0;
+	handleResize = () => {
+		this.setState({ windowWidth: window.innerWidth });
+	}
 
+	componentDidMount() {
+		window.addEventListener('resize', this.handleResize);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.handleResize);
+	}
+
+	handleToggleOffcanvas = () => {
+		this.setState(prevState => ({ showOffcanvas: !prevState.showOffcanvas }));
+	}
+
+	render() {
+		const { user, isAuthenticated } = this.props.auth0;
+		const { windowWidth, showOffcanvas } = this.state;
 
 		return (
 			<div className='header'>
-				<div className='nav-bar'>
-					<div className='nav-bar-background'>
-						<div className='nav-buttons-background'>
-							<Link className='nav-buttons' to="/" >Home</Link>
+				{windowWidth < 830 ? (
+					<>
+						<Offcanvas show={showOffcanvas} onHide={this.handleToggleOffcanvas}>
+							<Offcanvas.Header  closeButton>
+							</Offcanvas.Header>
+							<Offcanvas.Body>
+								<>
+									<div className='nav-bar'>
+											<div className='nav-buttons-background'>
+												<Link className='nav-buttons' to="/" >Home</Link>
+											</div>
+											<div className='nav-buttons-background' >
+												<Link className='nav-buttons' to="/Notebook" >Notebook</Link>
+											</div>
+									</div>
+									<div className='login-bar'>
+											{isAuthenticated ?<Link className='nav-buttons' to='/profile'>Profile</Link> : null}
+											{isAuthenticated ? <Logout/> : <Login/>}
+									</div>
+								</>
+							</Offcanvas.Body>
+						</Offcanvas>
+						<button className='mobile-menu' onClick={this.handleToggleOffcanvas}>
+							<div className="hamburger-line"></div>
+							<div className="hamburger-line"></div>
+							<div className="hamburger-line"></div>
+						</button>
+						<div className='LogoHolder'>
+							<img className='Logo' src={logo}/>
 						</div>
-						<div className='nav-buttons-background' >
-							<Link className='nav-buttons' to="/Notebook" >Notebook</Link>
+					</>
+				) : (
+					<>
+						<div className='nav-bar'>
+							<div className='nav-bar-background'>
+								<div className='nav-buttons-background'>
+									<Link className='nav-buttons' to="/" >Home</Link>
+								</div>
+								<div className='nav-buttons-background' >
+									<Link className='nav-buttons' to="/Notebook" >Notebook</Link>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-				<div className='LogoHolder'>
-					<img className='Logo' src={logo}/>
-				</div>
-				<div className='login-bar'>
-					<div className='login-bar-background'>
-						{isAuthenticated ? <p className='welcome'>Welcome {user.name.split(' ')[0]}!</p>: null}
-						{isAuthenticated ?<Link className='nav-buttons' to='/profile'>Profile</Link> : null}
-						{isAuthenticated ? <Logout/> : <Login/>}
-					</div>
-				</div>
+						<div className='LogoHolder'>
+							<img className='Logo' src={logo}/>
+						</div>
+						<div className='login-bar'>
+							<div className='login-bar-background'>
+								{isAuthenticated ? <p className='welcome'>Welcome {user.name.split(' ')[0]}!</p>: null}
+								{isAuthenticated ?<Link className='nav-buttons' to='/profile'>Profile</Link> : null}
+								{isAuthenticated ? <Logout/> : <Login/>}
+							</div>
+						</div>
+					</>
+				)}
 			</div>
 		)
 	}
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ class Welcome extends React.Component {
 	render() {
 		return (
 			<div className='welcome-background'>
-				<h2 className='welcome-text'>Welcome to S'more Fun! Please login to explore your state's campsites!</h2>
+				<h2 className='welcome-text'>Welcome to S'more Fun! Please login to enable your notebook!</h2>
 			</div>
 		);
 	}
